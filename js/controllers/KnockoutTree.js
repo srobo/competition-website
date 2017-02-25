srobo-legacy/srweb

Fix #2432: Colour code the knockout corners
While the layout for the view already matched the arena, this wasn't
clear unless you knew it to be. Adding colours to the corners makes
it immediately clear which corner is which.

Change-Id: I9cca6c64c589b92cd9e789579029eb03d8842d12

js/controllers/KnockoutTree.js
@@ -1,10 +1,15 @@
 
 var app = angular.module('app', ["competitionFilters", "competitionResources"]);
 
-app.controller("KnockoutTree", function($scope, Arenas, KnockoutMatches, LeagueScores, MatchesFactory, State, Teams) {
+app.controller("KnockoutTree", function($scope, Arenas, Corners, KnockoutMatches, LeagueScores, MatchesFactory, State, Teams) {
 
     $scope.unknowable = UNKNOWABLE_TEAM;
     var KNOCKOUT_TYPE = "knockout";
+
+    $scope.corners = [];
+    Corners.load(function(cornerId, corner) {
+        $scope.corners[cornerId] = corner;
+    });
 
     Teams.follow(function(teams) {
         $scope.teams = teams;

srobo/competition-website

js/controllers/KnockoutTree.js
@@ -1,10 +1,15 @@
 
 var app = angular.module('app', ["competitionFilters", "competitionResources"]);
 
-app.controller("KnockoutTree", function($scope, Current, KnockoutMatches, LastScoredMatch, MatchPeriods, State, Teams) {
+app.controller("KnockoutTree", function($scope, Corners, Current, KnockoutMatches, LastScoredMatch, MatchPeriods, State, Teams) {
 
     $scope.unknowable = UNKNOWABLE_TEAM;
     var KNOCKOUT_TYPE = "knockout";
+
+    $scope.corners = [];
+    Corners.load(function(cornerId, corner) {
+        $scope.corners[cornerId] = corner;
+    });
 
     var knockout_period = {};
 